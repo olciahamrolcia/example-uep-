73419fe59d81c23dd35841923b50f2e52d4b90fc
Ola Hamrol <ola.hamrol@gmail.com>
2023-04-16 17:07:53 +0200

    Added square

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -9,3 +9,12 @@ obwod = a + b + c
 pole = int((h * a) / 2)
 
 print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
+
+#square
+
+a = 10
+obwod = a*4
+
+pole = a**2
+
+print("Obwod kwadratu wynosi" + str(obwod) + ", a pole wynosi" + str(pole) + ".")
