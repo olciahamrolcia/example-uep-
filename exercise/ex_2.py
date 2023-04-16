6ac4c302561cf5c5e688cae029c3a075dbfdb232
Ola Hamrol <ola.hamrol@gmail.com>
2023-04-16 17:11:52 +0200

    Added rectangle

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -17,4 +17,16 @@ obwod = a*4
 
 pole = a**2
 
-print("Obwod kwadratu wynosi" + str(obwod) + ", a pole wynosi" + str(pole) + ".")
+print("Obwod kwadratu wynosi " + str(obwod) + ", a pole wynosi " + str(pole) + ".")
+
+
+#rectangle
+
+a = 10
+b = 20
+
+obwod = a*2 + b*2
+
+pole = a*b
+
+print("Obwod prostokata wynosi " + str(obwod) + ", a pole wynosi " + str(pole) + ".")
